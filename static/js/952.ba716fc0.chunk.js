0879f66000f218b1c5ae18fcfb8908c3d57fae1c
robertsw22 <robertsw@stanford.edu>
2026-08-31 14:36:11 -0700

    Updates

---
+++ static/js/952.ba716fc0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcompanies_by_industry=self.webpackChunkcompanies_by_industry||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.ba716fc0.chunk.js.map
